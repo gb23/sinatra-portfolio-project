ff72835e63fb4966f68a778e46099e9a8f8824a7
gb23 <gregbenj@hotmail.com>
2017-08-04 17:01:45 -0700

    add multiple sort categories

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,86 +1,113 @@
-require 'erb'
-
 class ItemsController < ApplicationController
-    @sortchoice = "date_expires"
-
+ 
     get '/items' do
+
         if logged_in?
             @user = current_user
             @duplicate_names = []
             @fridges_hash = {}
-            @fridges_hash = create_fridges_hash(@user, @sortchoice)
+            @fridges_hash = create_and_sort_fridges_hash(@user)
             erb :'items/index'
         else
             erb :'users/failure'
         end
     end
-
+    
     get '/items/sort_date_expires' do
-        @sortchoice = "date_expires"
+        session[:sort_choice] = "date_expires"
         redirect to '/items'
     end
 
     get '/items/sort_date_sell_by' do
-        @sortchoice = "date_sell_by"
+        session[:sort_choice] = "date_sell_by"
         redirect to '/items'
     end
 
     get '/items/sort_name' do
-        @sortchoice = "name"
+        session[:sort_choice] = "name"
         redirect to '/items'
     end
 
     get '/items/sort_quantity' do
-        @sortchoice = "quantity"
+        session[:sort_choice] = "quantity"
         redirect to '/items'
     end
 
     get '/items/sort_category' do
-        @sortchoice = "category"
+        session[:sort_choice] = "category"
         redirect to '/items'
     end
 
-     get '/items/sort_weight' do
-        @sortchoice = "weight"
+     get '/items/sort_grams' do
+        session[:sort_choice] = "grams"
         redirect to '/items'
-    end
-
-
-    
-
-    get '/items/:slug' do
-        if logged_in?
-            @user = current_user
-binding.pry
-        @item_grouping = @user.items.select_by_slug(params[:slug])
-
-        # else
-
-        end
     end
 
     helpers do
-        def create_fridges_hash(user, sortchoice)
+        def create_and_sort_fridges_hash(user)
             duplicate_names = []
             fridges_hash = {}
             alphabetical_fridges = []
-            alphabetical_fridges = user.fridges.sort_by{|fridge| fridge.name}
+            alphabetical_fridges = user.fridges.order(:name)
             alphabetical_fridges.each do |fridge|
                 fridges_hash[fridge] = []
-                sorted_items = fridge.items.sort_by{|item| item.send(sortchoice) }
+                sorted_items = sort_items(fridge)
                 sorted_items.each do |item| 
                     if !duplicate_names.find{|slug| slug == item.slug}
+
                         item_grouping = user.items.select_by_slug(item.slug)
+                        item_grouping.each do |i|
+                            i.quantity = item_grouping.size
+                            i.save
+                        end
                         item[:quantity] = item_grouping.size
+
                         if item_grouping.size > 1
                             duplicate_names << item.slug
                         end
                         fridges_hash[fridge] << item
                     end
                 end
-            end 
-            fridges_hash
-        end
+            end
+         fridges_hash
+        end 
     end
 
+    def sort_items(fridge)
+        if !session[:sort_toggle].nil?
+            if session[:visits] > 0 && session[:visits] < 3
+                session[:visits] = session[:visits] + 1
+            else
+                session[:sort_toggle] == 0 ? session[:sort_toggle] = 1 : session[:sort_toggle] = 0
+                session[:visits] = 1
+            end
+        end
+        if session[:sort_choice] != "date_expires" && session[:sort_choice] != "date_sell_by" && !session[:sort_choice].nil?
+            if session[:sort_toggle] == 1
+                fridge.items.order(session[:sort_choice] + " " + "DESC") 
+            else
+                fridge.items.order(session[:sort_choice])
+            end
+        elsif session[:sort_choice] == "date_sell_by"
+            if session[:sort_toggle] == 1
+                fridge.items.order('CASE WHEN date_sell_by IS NULL THEN 1 ELSE 0 END, date_sell_by DESC')
+            else
+                fridge.items.order('CASE WHEN date_sell_by IS NULL THEN 1 ELSE 0 END, date_sell_by')
+            end
+        else
+            #default
+            if session[:sort_toggle].nil?
+                session[:visits] = 1
+                session[:sort_toggle] = 0
+                session[:sort_choice] = "date_expires"
+                fridge.items.order('CASE WHEN date_expires IS NULL THEN 1 ELSE 0 END, date_expires')
+            else
+                if session[:sort_toggle] == 1
+                    fridge.items.order('CASE WHEN date_expires IS NULL THEN 1 ELSE 0 END, date_expires DESC')
+                else
+                    fridge.items.order('CASE WHEN date_expires IS NULL THEN 1 ELSE 0 END, date_expires')
+                end
+            end
+        end
+    end
 end
