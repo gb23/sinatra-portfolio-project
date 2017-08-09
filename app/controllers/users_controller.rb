43886f2a9256e436c2bffefbdea175943f9f8926
gb23 <gregbenj@hotmail.com>
2017-08-09 09:08:00 -0700

    add logut

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,27 @@ class UsersController < ApplicationController
         erb :'users/login'
     end
 
-    get '/users/:slug' do
-        @user = User.select_by_slug(params[:slug]).first
-        erb :'users/show_user'
-    end
+
 
     get '/users/create_user' do
         erb :'users/create_user'
     end
 
-    get '/logout' do
+    get '/users/logout' do
         if logged_in?
             session.destroy
-            redirect to 'users/login'
+            redirect to '/users/login'
         else
             redirect to '/'
+        end
+    end
+
+    get '/users/:slug' do
+        @user = User.select_by_slug(params[:slug]).first
+        erb :'users/show_user'
     end
 
     post '/users/create_user' do
-binding.pry
         user = User.new(username: params[:name], password: params[:password], first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
         if user.save  #only true if user has typed in password.  If blank, won't be true
             #!User.all.find {|usr| usr == user} && 
@@ -40,7 +42,6 @@ binding.pry
             session[:user_id] = user.id
             redirect to '/items'
         else
-binding.pry
             erb :'users/failure'
         end
     end
